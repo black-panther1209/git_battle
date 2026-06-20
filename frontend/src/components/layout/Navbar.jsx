409bf436b4009830e95413593e36a36d67bfcb1a
Shristi <shris1209@gmail.com>
2026-06-20 10:37:24 +0530

    Setup React frontend and initial landing page

---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Git Battle ⚔️</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/layout/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1>Git Battle</h1>
+      <p>Analyze. Compare. Improve.</p>
+    </>
+  );
+}
+
+export default Home;
